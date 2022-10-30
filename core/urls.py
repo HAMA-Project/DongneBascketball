cb3bfb3735e2af7b416454585f49f55ca70a2368
hayongwoon <a01046641857@gmail.com>
2022-10-30 17:17:25 +0900

    add defendency

--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ninja import NinjaAPI
+
+api = NinjaAPI()
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/", api.urls),
 ]
