c3e0dba0a0e6779546cdc5b495455983470ac9fe
hayongwoon <a01046641857@gmail.com>
2022-10-30 22:48:13 +0900

    fix

--- core/urls.py
+++ core/urls.py
@@ -15,12 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ninja import NinjaAPI
-
-api = NinjaAPI()
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", api.urls),
 ]
