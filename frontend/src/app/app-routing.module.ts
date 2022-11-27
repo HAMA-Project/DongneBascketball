1901cb5672904d8b2477e6523cf2b7a8366a51f4
mahanbeom <akgksqja@gmail.com>
2022-11-27 18:40:47 +0900

    freeboard detail component fix

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,12 +5,18 @@ import { FreeboardCreateComponent } from './freeboard/freeboard-create/freeboard
 import { GuestAnnouncementComponent } from './guest-announcement/guest-announcement.component';
 import { HomeComponent } from './home/home.component';
 import { QuestionAnswerComponent } from './question-answer/question-answer.component';
+import { FreeboardDetailComponent } from './freeboard/freeboard-detail/freeboard-detail.component';
+import { FreeboardResolverService } from './freeboard/freeboard-resolve.service';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'guest-announcement', component: GuestAnnouncementComponent },
   { path: 'freeboard', component: FreeboardComponent },
   { path: 'freeboard/create', component: FreeboardCreateComponent },
+  { 
+    path: 'freeboard/:freeboardPost-id', component: FreeboardDetailComponent,
+    resolve: { freeboardPost: FreeboardResolverService }
+  },
   { path: 'question-answer', component: QuestionAnswerComponent },
   { path: '',   redirectTo: '/home', pathMatch: 'full' },
 ];
