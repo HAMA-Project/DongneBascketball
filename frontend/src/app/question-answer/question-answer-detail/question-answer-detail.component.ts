dbd7c1696f1170b01f0a8924efbe115879d15911
mahanbeom <akgksqja@gmail.com>
2022-11-29 19:24:21 +0900

    Q & A detail, create fix

--- frontend/src/app/question-answer/question-answer-detail/question-answer-detail.component.ts
+++ frontend/src/app/question-answer/question-answer-detail/question-answer-detail.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { QuestionAnswerPost } from 'src/app/domain/question-answer';
 
 @Component({
   selector: 'app-question-answer-detail',
   templateUrl: './question-answer-detail.component.html',
-  styleUrls: ['./question-answer-detail.component.scss']
+  styleUrls: ['./question-answer-detail.component.scss'],
 })
 export class QuestionAnswerDetailComponent implements OnInit {
+  QAPost?: QuestionAnswerPost;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute) {
+	this.QAPost = route.snapshot.data['QAPost'];
+	console.log(this.QAPost);
   }
 
+  ngOnInit(): void {}
 }
