bf343433c1d34c87359dd69e6e80a16d613388b2
mahanbeom <akgksqja@gmail.com>
2022-12-02 19:43:03 +0900

    fix: app-routing.module.ts

--- frontend/src/app/freeboard/freeboard-resolve.service.ts
+++ frontend/src/app/freeboard/freeboard-resolve.service.ts
@@ -1,19 +1,24 @@
-import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
-import { Observable } from "rxjs";
-import { FreeBoardPost } from "../domain/freeboard";
-import { freeBoardPostSampleData } from "../sample-data";
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  Resolve,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { FreeBoardPost } from '../domain/freeboard';
+import { freeBoardPostSampleData } from '../sample-data';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class FreeboardResolverService implements Resolve<FreeBoardPost> {
+  constructor() {}
 
-    constructor() {
-    }
-
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<FreeBoardPost> {
-		const postId = route.params['freeboardPost-id'];
-		return freeBoardPostSampleData[postId -1] as any;
-    };
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<FreeBoardPost> {
+    const postId = route.params['id'];
+    return freeBoardPostSampleData[postId - 1] as any;
+  }
 }
