8aaeb2d32f167aaaa6c2fb171e23a9744009b525
mahanbeom <akgksqja@gmail.com>
2022-12-18 18:08:26 +0900

    fix: guest announcement detail

--- frontend/src/app/guest-announcement/guest-announcement-detail/guest-announcement-detail.component.ts
+++ frontend/src/app/guest-announcement/guest-announcement-detail/guest-announcement-detail.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GuestPost } from 'src/app/domain/guest';
 
 @Component({
-  selector: 'app-guest-announcement-detail',
-  templateUrl: './guest-announcement-detail.component.html',
-  styleUrls: ['./guest-announcement-detail.component.scss']
+	selector: 'app-guest-announcement-detail',
+	templateUrl: './guest-announcement-detail.component.html',
+	styleUrls: ['./guest-announcement-detail.component.scss']
 })
 export class GuestAnnouncementDetailComponent implements OnInit {
 
-  constructor() { }
+	guestPost?: GuestPost;
 
-  ngOnInit(): void {
-  }
+	constructor(
+		private route: ActivatedRoute
+	) {
+		this.guestPost = route.snapshot.data['GuestPost'];
+		console.log(this.guestPost);
+	}
+
+	ngOnInit(): void {
+	}
 
 }
