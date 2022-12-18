04c37046567b0ee458a1e981004d5c525e8e722a
mahanbeom <akgksqja@gmail.com>
2022-12-18 18:12:00 +0900

    feat: (WIP) Add Q & A post Edit, Delete

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -20,7 +20,7 @@ import { QuestionAnswerCreateComponent } from './question-answer/question-answer
 import { QuestionAnswerDetailComponent } from './question-answer/question-answer-detail/question-answer-detail.component';
 import { GuestAnnouncementCreateComponent } from './guest-announcement/guest-announcement-create/guest-announcement-create.component';
 import { GuestAnnouncementDetailComponent } from './guest-announcement/guest-announcement-detail/guest-announcement-detail.component';
-import { ConfirmDialog, DeleteDialog } from './common/dialogs/dialog/dialog.component';
+import { ConfirmDialog, DeleteDialog } from './common/dialogs/dialog.component';
 
 const dialogs = [
   SignInDialog,
