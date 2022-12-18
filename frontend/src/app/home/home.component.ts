15742132272d5407fc0135ba68d082165481c1bb
mahanbeom <akgksqja@gmail.com>
2022-12-18 18:10:33 +0900

    fix: global table row css, home component

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,27 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { freeBoardPostSampleData, guestPostSampleData, questionAnswerPostSampleData } from '../sample-data';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { FreeBoardPost } from '../domain/freeboard';
+import { GuestPost } from '../domain/guest';
+import { QuestionAnswerPost } from '../domain/question-answer';
+import {
+  freeBoardPostSampleData,
+  guestPostSampleData,
+  questionAnswerPostSampleData,
+} from '../sample-data';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
+  guestPostDataSource: MatTableDataSource<GuestPost> =
+    new MatTableDataSource<GuestPost>([]);
+  freeBoardPostDataSource: MatTableDataSource<FreeBoardPost> =
+    new MatTableDataSource<FreeBoardPost>([]);
+  QAPostDataSource: MatTableDataSource<QuestionAnswerPost> =
+    new MatTableDataSource<QuestionAnswerPost>([]);
 
-  guestPostDisplayedColumns: string[] = ['title', 'region', 'match_date', 'status', 'writer'];
-  guestPostDataSource = guestPostSampleData;
-
+  guestPostDisplayedColumns: string[] = [
+    'title',
+    'region',
+    'match_date',
+    'status',
+    'writer',
+  ];
   freeBoardPostDisplayedColumns: string[] = ['title', 'created_date', 'writer'];
-  freeBoardPostDataSource = freeBoardPostSampleData;
+  QAPostDisplayedColumns: string[] = ['title', 'created_date', 'writer'];
 
-  questionAnswerPostDisplayedColumns: string[] = ['title', 'created_date', 'writer'];
-  questionAnswerPostDataSource = questionAnswerPostSampleData;
-
-
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.guestPostDataSource.data = guestPostSampleData;
+    this.freeBoardPostDataSource.data = freeBoardPostSampleData;
+    this.QAPostDataSource.data = questionAnswerPostSampleData;
   }
 
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {}
+
+  ngOnDestroy(): void {
+    if (this.guestPostDataSource) this.QAPostDataSource.disconnect();
+    if (this.freeBoardPostDataSource) this.QAPostDataSource.disconnect();
+    if (this.QAPostDataSource) this.QAPostDataSource.disconnect();
+  }
 }
