cd555e5032abcc64b87a14a95bb5a43e431eda68
hayongwoon <a01046641857@gmail.com>
2023-01-16 00:11:33 +0900

    add python-dotenv

--- core/settings.py
+++ core/settings.py
@@ -14,6 +14,10 @@ import typing
 from pathlib import Path
 
 import environ
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
